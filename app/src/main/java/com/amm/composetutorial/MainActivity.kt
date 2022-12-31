397088f9d8f63a1bc855019f379172cafd6a2c2c
mmrach <mmrach@gmail.com>
2022-12-31 11:21:09 +0100

    Importada Imagen y mostrada en fila

--- app/src/main/java/com/amm/composetutorial/MainActivity.kt
+++ app/src/main/java/com/amm/composetutorial/MainActivity.kt
@@ -3,8 +3,12 @@ package com.amm.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +24,17 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message){
-    Text(text = msg.author)
-    Text(text = msg.body)
+    Row() {
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "Contact profile picture"
+        )
+        Column() {
+            Text(text = msg.author)
+            Text(text = msg.body)
+        }
+
+    }
 }
 
 @Preview
